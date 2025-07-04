da56975f5916743097a87d05615ecfbb08ce628a
matiasediez <matiasediez@gmail.com>
2025-07-03 21:51:41 -0300

    Llamo al modal desde las cards

--- src/components/cards.js
+++ src/components/cards.js
@@ -1,19 +1,24 @@
 import { getProducts } from "../api/api.js";
+import { createModal } from "../components/modal.js";
 
 export function createCards() {
     let containerCards = document.querySelector('#list-products');
     getProducts().then((data) => {
+
+        window.showDetails = (p) => {
+            createModal(p);
+        };
+
         data.forEach((p) => {
             let template = `<div class="col">
                             <div class="card">
                                 <img src="${p.images[0]}" class="card-img-top" alt="${p.title}">
                                 <div class="card-body">
                                     <h5 class="card-title">${p.title}</h5>
-                                    <p class="card-text">${p.description}</p>
+                                    <button type='button' class='btn btn-primary' onclick='showDetails(${JSON.stringify(p)})' id="${p.id}">Ver detalles</buttom>
                                 </div>
                             </div>
                         </div>`;
-
             containerCards.innerHTML += template;
         })
     });
