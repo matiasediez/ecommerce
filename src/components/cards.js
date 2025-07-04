aa457e83883158a67605d24286f73182e0848811
matiasediez <matiasediez@gmail.com>
2025-07-04 15:15:08 -0300

    Le doy formato a las cards, centradas y alineadas

--- src/components/cards.js
+++ src/components/cards.js
@@ -9,22 +9,15 @@ export function createCards() {
       data.forEach(p => {
         const payload = encodeURIComponent(JSON.stringify(p));
         const template = `
-          <div class="col">
-            <div class="card">
-              <img src="${p.images[0]}" class="card-img-top" alt="${p.title}">
-              <div class="card-body">
-                <h5 class="card-title">${p.title}</h5>
-                <button
-                  type="button"
-                  class="btn btn-primary btn-detail"
-                  data-payload="${payload}"
-                  id="btn-${p.id}"
-                >
-                  Ver detalles
-                </button>
-              </div>
-            </div>
-          </div>`;
+            <div class="col">
+                <div class="card h-100">
+                <img src="${p.images[0]}" class="card-img-top" alt="${p.title}">
+                    <div class="card-body d-flex flex-column justify-content-between align-items-center text-center h-100">
+                    <h5 class="card-title clamp-2">${p.title}</h5>
+                    <button type="button" class="btn btn-primary btn-detail" data-payload="${payload}" id="btn-${p.id}">Ver detalles</button>
+                    </div>
+                </div>
+            </div>`;
         containerCards.innerHTML += template;
       });
 
