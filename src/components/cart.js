c2ee1d27bc29f5a342225d8b96edb424623aba74
matiasediez <matiasediez@gmail.com>
2025-07-03 23:38:04 -0300

    Muestro cartel al agregar al carrito

--- src/components/cart.js
+++ src/components/cart.js
@@ -2,5 +2,14 @@ export function addToCart(product) {
   const cart = JSON.parse(localStorage.getItem('cart')) || [];
   cart.push(product);
   localStorage.setItem('cart', JSON.stringify(cart));
-  //alert(`${product.title} agregado al carrito!`);
+
+  const toastEl = document.getElementById('cartToast');
+  const toastBody = toastEl.querySelector('.toast-body');
+
+  toastBody.textContent = `"${product.title}" agregado al carrito. 🛒`;
+
+  const bsToast = new bootstrap.Toast(toastEl, {
+    delay: 1500
+  });
+  bsToast.show();
 }
